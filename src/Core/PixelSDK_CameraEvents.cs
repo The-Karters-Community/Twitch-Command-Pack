1686becf823be3a0569e3738aca5af248ea5f9be
Kévin Bibollet <bibollet.kevin@gmail.com>
2024-05-29 01:42:11 +0200

    added a line to not inject code to PixelSDK_CameraEvents methods if cmd is not enabled

--- src/Core/PixelSDK_CameraEvents.cs
+++ src/Core/PixelSDK_CameraEvents.cs
@@ -2,6 +2,7 @@
 // https://github.com/MKsKartersModTeam/MKsKartersMods/blob/2a4b984915356b7d7f66e02e64318c42f3ccf514/Mods/MirrorMode.cs#L53
 
 using HarmonyLib;
+using TwitchCommandPack.Commands;
 using UnityEngine;
 
 namespace TwitchCommandPack.Core;
@@ -9,6 +10,11 @@ namespace TwitchCommandPack.Core;
 [HarmonyPatch(typeof(PixelSDK_CameraEvents), nameof(PixelSDK_CameraEvents.OnPreCull))]
 public static class PixelSDK_CameraEvents__OnPreCull {
     private static void Postfix(PixelSDK_CameraEvents __instance) {
+        // Do not execute what's after is the command is not active.
+        if (!ReverseScreenCommand.isReverseScreenEnabled) {
+            return;
+        }
+
         // Invert the camera projection matrix across the X-axis
         var camera = __instance.parentCamera.unityCamera;
         var mat = camera.projectionMatrix;
@@ -20,14 +26,24 @@ public static class PixelSDK_CameraEvents__OnPreCull {
 
 [HarmonyPatch(typeof(PixelSDK_CameraEvents), nameof(PixelSDK_CameraEvents.OnPreRender))]
 public static class PixelSDK_CameraEvents__OnPreRender {
-    private static void Postfix(PixelSDK_CameraEvents __instance) {
+    private static void Postfix() {
+        // Do not execute what's after is the command is not active.
+        if (!ReverseScreenCommand.isReverseScreenEnabled) {
+            return;
+        }
+
         GL.invertCulling = true;
     }
 }
 
 [HarmonyPatch(typeof(PixelSDK_CameraEvents), nameof(PixelSDK_CameraEvents.OnPostRender))]
 public static class PixelSDK_CameraEvents__OnPostRender {
-    private static void Postfix(PixelSDK_CameraEvents __instance) {
+    private static void Postfix() {
+        // Do not execute what's after is the command is not active.
+        if (!ReverseScreenCommand.isReverseScreenEnabled) {
+            return;
+        }
+
         GL.invertCulling = false;
     }
 }
